c5170ee205834a49b3aada633ae907023cab56c4
Daniel Esquivias <danielesquivias@bhm-ms-sserv-04.medseek.com>
2014-09-12 08:20:15 -0700

    Initial commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterAMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func mailButtonTouchUpInside(sender: UIButton) {
+        // Code will evaluate when we touch up inside the button
+        
+        messageLabel.hidden = false
+        messageLabel.text = enterAMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        enterAMessageTextField.text = ""
+        enterAMessageTextField.resignFirstResponder() // Hide the keyboard from enterAMessage
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+
+    }
 
 }
 
